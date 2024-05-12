deb3e66131912084502f146391565ee95ffdf684
stellaksarpong <stellaksarpong@gmail.com>
2024-05-12 11:28:29 +0000

    Initial commit

--- server.js
+++ server.js
@@ -4,3 +4,13 @@
 const { PrismaClient } = require('@prisma/client');
 // Create a new instance of Prisma Client
 const prisma = new PrismaClient();
+
+const express=require('express')
+const app=express()
+const port=3000
+app.get('/',(res,send)=>{
+    res.send("Hello KS_KART")
+})
+app.listen(port,() =>{
+    console.log(`hello listen to my ${port}`)
+})
